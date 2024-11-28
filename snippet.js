e8a0d9ad19d0cb0f4daa49e66f37281e73dc9043
Dashrath <dashrath.kadam@mai.in>
2024-11-28 14:58:39 +0530

    coading

--- snippet.js
+++ snippet.js
@@ -1,1 +1,33 @@
-//this is the Javascript file
+function add(numbers) {
+    if (!numbers) return 0;
+
+    let delimiter = /,|\n/;
+    let otherDelimiter = numbers.match(/^\/\/(.+)\n/);
+
+    console.log(otherDelimiter);
+    if (otherDelimiter) {
+        delimiter = new RegExp(otherDelimiter[1]); 
+        numbers = numbers.substring(otherDelimiter[0].length);
+    }
+
+    const numberArray = numbers.split(delimiter).map(num => Number(num.trim()));
+
+
+    const negativeNumbers = numberArray.filter(num => num < 0);
+    if (negativeNumbers.length > 0) {
+        throw new Error(`Negative numbers not allowed: ${negativeNumbers.join(', ')}`);
+    }
+
+    return numberArray.reduce((sum, num) => sum + num, 0);
+}
+
+try {
+    console.log(add("")); 
+    console.log(add("1")); 
+    console.log(add("1,5")); 
+    console.log(add("1\n2,3")); 
+    console.log(add("//;\n1;2")); 
+    console.log(add("1,-2,3")); 
+} catch (error) {
+    console.error(error.message);
+}
